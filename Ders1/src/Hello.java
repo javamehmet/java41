da58932e605dd36876ceb1fc439e355f67883a3a
hokumus86 <vektorel@lab9-ogr>
2019-03-13 17:49:16 +0200

    first commit

---
+++ Ders1/src/Hello.java
@@ -0,0 +1,91 @@
+
+public class Hello {
+
+	public static void main(String args[]) {
+
+		
+		/***
+		 * 
+		 * 
+		 * Veri 
+		 * 
+		 * Tam Sayýlarý
+		 *  10 12 100 
+		 *  
+		 *  Kelime karakter ali veli java yaz ciz 
+		 *  
+		 *  kesirli sayýlar 10.5 3.14 
+		 *  
+		 *  mantýksal veriler true false
+		 *  
+		 *  byte 01010101  8 bit 
+		 *  
+		 *  
+		 *  
+		 *  
+		 *  Tam Sayýlarý short 16bit 010100 int32bit  long 64bit
+		 *  
+		 *   Kesirli float 32 double 64   2,14 3,45
+		 *   
+		 *   boolean  true false
+		 *   
+		 *   byte 0101
+		 *   
+		 *   char 'k'  '?' '.'
+		 *  
+		 * short int long float double char byte boolean  Primitive Data Types Ýlkel veri tipleri   
+		 * 
+		 * 
+		 * veriTipi degiskenAdi=degeri;  float pi=3.14f;  % * 
+		 * 
+		 * 
+		 * 
+		 */
+	
+		short sayi=10;
+		
+		System.out.println("short tipinde veri sayi :"+sayi);
+		
+		int sayi2=15;
+		
+		System.out.println("int tipinde veri sayi2 :"+sayi2);
+		
+		long sayi3=30;
+		
+		System.out.println("long tipinde veri sayi3 :"+sayi3);
+		
+		float pi=3.14f;
+		
+		System.out.println("float tipinde veri pi :"+pi);
+		
+		double pi2=3.14;
+		
+		System.out.println("double tipinde veri pi2:"+pi2);
+		
+		char karakter='k';
+		
+		System.out.println("karakter tipinde veri karakter:"+karakter);
+		boolean varMi=false;
+		
+		System.out.println("boolean tipinde veri boolean :"+varMi);
+		
+		byte bit=15;
+		
+		System.out.println("byte tipinde veri bit :"+bit);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("asdas");
+		
+	
+	
+	}
+
+}
